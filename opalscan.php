7b3ea751fbc9632b4dd888870bde5af3e1106947
Black-Box\Angstrom <steve@pheriche.com>
2019-12-02 20:54:10 +0000

    more scoring

--- opalscan.php
+++ opalscan.php
@@ -19,115 +19,50 @@ include_once('includes/phua_add_wp_head.php' );
 */
 /*************************************************/
 
-add_action('admin_menu', 'register_opalscan_menu');
-function register_opalscan_menu() {
-	add_menu_page( 'Opal Site Scan', 'Opal Site Scan', 'manage_options', 'opal-site-scan', 'phua_admin_page_output' );
-	#add_action('admin_init', 'save_log_page_items', 10); // if you need to save something weird this might be useful.
-}
-/*
-function phua_enqueue_scripts( ) {
-wp_enqueue_script('jquery-ui-datepicker');
-	wp_enqueue_style('jquery-ui-css', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css');
-	wp_enqueue_script( 'datepicker_script', plugin_dir_url( __FILE__ ) . 'includes/datepicker.js' );
-
-	wp_enqueue_script( 'log_search', plugin_dir_url( __FILE__ ) . 'includes/pher-logsearch-admin.js', array( 'jquery' ),false,true );
-	wp_localize_script( 'log_search', 'ph_log_search', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ),  ));
-	wp_register_style( 'phua_admin_css', plugin_dir_url( __FILE__ ) . '/includes/phua_logging_admin.css', false, '1.0.0' );
-	wp_enqueue_style( 'phua_admin_css' );
-}
-
-add_action('admin_enqueue_scripts', 'phua_enqueue_scripts');
-*/
-
-add_action( 'wp_ajax_do_some_ajax', 'phua_log_ajax_output');
-
-
-function phua_admin_page_output(){
-		echo '<div class="wrap"><div id="icon-edit-pages" class="icon32"></div><h2>Opal Site Scanner</h2>';
-
-			echo('<div class="opalscanbarholder">Scanning <div class="opalscanbar"></div></div>');
-
-		echo('<form method="post" id="log_export_form" >');
-		 echo('<hr>');
-		echo('<h3 id="loggingdateheader" class="loggingsectionheaditem">Date Range</h3>');
-		 $time = strtotime("-1 year", time());
- 		 $a_year_ago = date("Y-m-d", $time);
-		 $today = date("Y-m-d");
-
-		/*
-		$screen = get_current_screen();
-		echo('<pre>');print_r($screen );echo('</pre>');
-		*/
-		 $atts = array('title'=>'Authorities','formslug'=>'organisation');
-		//display_logging_row($atts);
-
-		 $atts = array('title'=>'Resources','formslug'=>'resources');
-		//display_logging_row($atts);
-
-		echo '<input type="hidden" name="download-login-log" value="true" >';
-		echo (wp_nonce_field('ualog_download'));
-
-		echo '<p><input type="submit" name="submit" id="submit" class="button bigwhitebutton" value="Export to Excel"></p>';
-		echo(' </form');
-		echo('</div>');
-
-
-		displayPluginsVersions();
-}
-
-
-
-#
-if(is_admin()) { // make sure, the following code runs in back end
+if(is_admin()) { // make sure, the following code runs only in the back end
 
 	if (!function_exists('plugins_api')) {
 		require_once( ABSPATH . 'wp-admin/includes/plugin-install.php' );
 	}
 
-		$plugin_vuln_score=0;
+		$vuln_score_plugin_outdated =
+		$vuln_score_plugin_noupdates =
+		$vuln_score_plugin_amount =
+		$vuln_score_php_version =
+		$vuln_score_sql_version =
+		$vuln_score_wp_version =
+		$vuln_score_https =
+		 0;
+
     // returns version of the plugin represented by $slug, from repository
-
     function getPluginVersionFromRepository($slug) {
-			/*
-			$url = "https://api.wordpress.org/plugins/info/1.2/?action=plugin_information&request[slugs][]={$slug}";
-			$response = wp_remote_get($url); // WPOrg API call
-			$plugins = json_decode($response['body']);
-*/
-
 				$call_api = plugins_api( 'plugin_information', array( 'slug' => $slug , 'version' => true,) );
-
-			/*echo('<pre>');
-			print_r($plugins);
-			echo('</pre>');*/
-			        // traverse $response object
-			        /*foreach($plugins as $key => $plugin) {
-			            $version = $plugin->version;
-									//$versions = $plugin->versions;
-								//	$last_updated =$plugin->last_updated;
-			        }*/
-
-			    return $call_api;
+			  return $call_api;
     }
 
     // dashboard widget's callback
-    function displayPluginsVersions() {
+    function displayVulnScan() {
         $allPlugins = get_plugins(); // associative array of all installed plugins
         $activePlugins = get_option('active_plugins'); // simple array of active plugins
 
+				$today= new DateTime();
+				$datetime2 = new DateTime('2009-10-13');
 
 $thead = <<<THEAD
 	<table width="100%">
 	<thead>
 	<tr>
 	<th width="20%" style="text-align:left">Plugin</th>
-	<th width="20%" style="text-align:left">currVer</th>
-	<th width="20%" style="text-align:left">repoVer</th>
+	<th width="20%" style="text-align:left">Installed Version</th>
+	<th width="20%" style="text-align:left">Available</th>
 	<th width="20%" style="text-align:left">WARN</th>
+	<th width="20%" style="text-align:left">Outdated</th>
 	</tr>
 	</thead>
 	<tbody>
 THEAD;
 echo $thead;
+
         // traversing $allPlugins array
         foreach($allPlugins as $key => $value) {
           //  if(in_array($key, $activePlugins)) { // display active only
@@ -139,28 +74,108 @@ echo $thead;
                 // get newest version of active plugin from repository
                 $call_api = getPluginVersionFromRepository($slug);
 								$repoversion = $call_api->version;
+								$last_updated = $call_api->last_updated;
 
+								$last_updated_date = new DateTime($last_updated );
                 echo "<td>{$repoversion}</td>";
 
-								if($repoversion>$value['Version']){		$issueflag = 'ISSUE';	$plugin_vuln_score +=1;
+								if($repoversion>$value['Version']){
+									$issueflag = 'ISSUE';
+									$vuln_score_plugin_outdated +=1;
 								}else{$issueflag ='';}
 
 								 echo "<td>$issueflag</td>";
+								 $interval = $today->diff($last_updated_date);
+								 $intervalstring= $interval->format('%R%a');
+								 $intervalINT=intval($intervalstring);
+
+								 if ($intervalINT <= -365){
+									 $datewarn="WARN";
+									 $vuln_score_plugin_noupdates +=1;
+								 }else{$datewarn="";}
+
+								 echo '<td>'.$intervalstring.' '.$datewarn.'</td>';
                 echo '</tr>';
           //  }
         }
         echo '</tbody>';
         echo '</table>';
-				echo '<b>Plugin Vulnerability Score = '.$plugin_vuln_score.'</b>';
+				echo '<h3>Plugin Vulnerability Score = '.$vuln_score_plugin_outdated + $vuln_score_plugin_noupdates .'</h3>';
     }
 
-    // widget's registration
-    function fpwAddDashboardWidget() {
-        wp_add_dashboard_widget(
-            'active_plugins_versions', // widget's ID
-            'Active Plugins Versions', // widget's title
-            'activePluginsVersions'    // widget's callback (content)
-        );
-    }
-    add_action('wp_dashboard_setup', 'fpwAddDashboardWidget');
+
+
+		add_action('admin_menu', 'register_opalscan_menu');
+		function register_opalscan_menu() {
+			add_menu_page( 'Opal Site Scan', 'Opal Site Scan', 'manage_options', 'opal-site-scan', 'phua_admin_page_output' );
+			#add_action('admin_init', 'save_log_page_items', 10); // if you need to save something weird this might be useful.
+		}
+		/*
+		function phua_enqueue_scripts( ) {
+		wp_enqueue_script('jquery-ui-datepicker');
+			wp_enqueue_style('jquery-ui-css', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css');
+			wp_enqueue_script( 'datepicker_script', plugin_dir_url( __FILE__ ) . 'includes/datepicker.js' );
+
+			wp_enqueue_script( 'log_search', plugin_dir_url( __FILE__ ) . 'includes/pher-logsearch-admin.js', array( 'jquery' ),false,true );
+			wp_localize_script( 'log_search', 'ph_log_search', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ),  ));
+			wp_register_style( 'phua_admin_css', plugin_dir_url( __FILE__ ) . '/includes/phua_logging_admin.css', false, '1.0.0' );
+			wp_enqueue_style( 'phua_admin_css' );
+		}
+
+		add_action('admin_enqueue_scripts', 'phua_enqueue_scripts');
+		*/
+
+		//add_action( 'wp_ajax_do_some_ajax', 'phua_log_ajax_output');
+
+
+		function phua_admin_page_output(){
+			echo '<div class="wrap"><div id="icon-edit-pages" class="icon32"></div><h2>Opal Site Scanner</h2>';
+			// Test if the query exists at the URL
+
+			//echo('<div class="opalscanbarholder">Scanning <div class="opalscanbar"></div></div>');
+			$scanurl=add_query_arg( 'scannow', 'true');
+			echo '<p><a class="button bigwhitebutton" href="'.$scanurl.'">SCAN</a></p>';
+			echo('<form method="post" id="log_export_form" >');
+			echo('<hr>');
+
+			$phpversion = phpversion();
+			$isSSL= is_ssl();
+
+			$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+			$SQLversion = ' | MySQL: ' . mysqli_get_server_info($connection);
+
+			echo 'PHP version '.$phpversion .' and SSL is '.$isSSL .' and SQL is '. $SQLversion;
+			echo('<hr>');
+
+			$time = strtotime("-1 year", time());
+			$a_year_ago = date("Y-m-d", $time);
+			$today = date("Y-m-d");
+
+		/*
+		$screen = get_current_screen();
+		echo('<pre>');print_r($screen );echo('</pre>');
+		*/
+
+			$scannow = $_GET['scannow']; // has the button been pressed, soon to be replaced by AJAX.
+
+			if(isset($scannow)){
+				echo('<div class="opalscanbarholder">Scanning <div class="opalscanbar"></div></div>');
+				displayVulnScan();
+			}
+
+				echo('</div>');// close the main edit page pane
+		}
+
+
+	    // widget's registration
+	    function opalAddDashboardWidget() {
+	        wp_add_dashboard_widget(
+	            'active_plugins_versions', // widget's ID
+	            'Active Plugins Versions', // widget's title
+	            'activePluginsVersions'    // widget's callback (content)
+	        );
+	    }
+		  //  add_action('wp_dashboard_setup', 'opalAddDashboardWidget');
+
+
 }
