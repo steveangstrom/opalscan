234674844023b0db3dc2c19004a072cffea86fae
Black-Box\Angstrom <steve@pheriche.com>
2019-12-03 18:05:03 +0000

    Ajax phase 1, updating area with HTML

--- includes/js/opal-scan.js
+++ includes/js/opal-scan.js
@@ -1,9 +1,14 @@
 jQuery(document).ready(function($) {
-
+  $('.opalscannow').click(function() {
+    alert( "Handler for .click() called." );
+    doScan();
+  });
     // We'll pass this variable to the PHP function example_ajax_request
     var fruit = 'Banana';
 
     // This does the ajax request
+
+    function doScan(){
     $.ajax({
         url: ajaxurl, // or example_ajax_obj.ajaxurl if using on frontend
         data: {
@@ -13,10 +18,12 @@ jQuery(document).ready(function($) {
         success:function(data) {
             // This outputs the result of the ajax request
             console.log(data);
+            $( "#opalscan_displayarea" ).html('SCAN PRETEND COMPLETED');
+
         },
         error: function(errorThrown){
             console.log(errorThrown);
         }
     });
-
+  }
 });
