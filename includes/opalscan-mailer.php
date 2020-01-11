d6e20179be54518edad9ebc5e29e63168763e1d4
Black-Box\Angstrom <steve@pheriche.com>
2020-01-11 01:46:16 +0000

    fix to a mail error

--- includes/opalscan-mailer.php
+++ includes/opalscan-mailer.php
@@ -2,14 +2,15 @@
 function opalreportmail() {
     if ( isset($_REQUEST) ) {
         $mailaction = $_REQUEST['mailaction'];
-        if ( $mailaction == 'sendreport' ) {
+        if ( $mailaction == 'sendmail' ) {
           $attachments = array(plugin_dir_url( __DIR__  ) . 'reports/scanlog.txt');
            $headers = 'From: My Name <pheriche@pheriche.com>' . "\r\n";
            $message = 'this is the test message that I am testing the testy plugin of';
            $mailout = wp_mail('steve@pheriche.com', 'subject', $message, $headers, $attachments);
+          echo 'we just tried to send a mail, this is step one ,' .$mailout;
         }
       #
-         echo 'we just tried to send a mail, this is step one ,' .$mailout;
+
     //  echo 'we just tried to send a mail, this is step one , the url = '.plugin_dir_url( __DIR__  ) . 'reports/scanlog.txt';
     }
    die();
